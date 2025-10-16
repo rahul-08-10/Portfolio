e5542da01dd1f947bd1cc770e5ade83c0ac3210f
rahul-08-10 <rahuljangid10498@gmail.com>
2025-10-17 00:50:36 +0530

    Add favicon link to RootLayout in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/briefcase-favicon.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
